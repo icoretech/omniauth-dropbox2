0583186662a740a9a9d4390fac55b6c60f4dd77e
Claudio Poli <masterkain@gmail.com>
2016-06-29 00:02:50 +0200

    add support for Dropbox API V2

--- lib/omniauth-dropbox2.rb
+++ lib/omniauth-dropbox2.rb
@@ -4,26 +4,26 @@ module OmniAuth
   module Strategies
     class Dropbox < OmniAuth::Strategies::OAuth2
       # Give your strategy a name.
-      option :name, "dropbox"
+      option :name, 'dropbox'
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
-      option :client_options, {
-        :site          => 'https://api.dropbox.com/2',
-        :authorize_url => 'https://www.dropbox.com/oauth2/authorize',
-        :token_url     => 'https://api.dropbox.com/oauth2/token'
-      }
+      option :client_options,
+             site: 'https://api.dropbox.com/2',
+             authorize_url: 'https://www.dropbox.com/oauth2/authorize',
+             token_url: 'https://api.dropbox.com/oauth2/token',
+             connection_opts: { headers: { user_agent: 'Omniauth-Dropbox2', accept: 'application/json', content_type: 'application/json' } }
 
       # These are called after authentication has succeeded. If
       # possible, you should try to set the UID without making
       # additional calls (if the user id is returned with the token
       # or as a URI parameter). This may not be possible with all
       # providers.
-      uid{ raw_info['uid'] }
+      uid { aw_info['account_id'] }
 
       info do
         {
-          :name => raw_info['display_name']
+          name: raw_info['name']['display_name']
         }
       end
 
@@ -34,7 +34,7 @@ module OmniAuth
       end
 
       def raw_info
-        @raw_info ||= access_token.get('users/get_current_account').parsed
+        @raw_info ||= access_token.post('users/get_current_account', body: nil.to_json).parsed
       end
     end
   end
--- lib/omniauth-dropbox2/version.rb
+++ lib/omniauth-dropbox2/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Box2
-    VERSION = "0.0.2"
+    VERSION = "1.0.0"
   end
 end
